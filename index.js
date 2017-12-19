27d3b6ce183ef896e5f05aec7452572f40b99487
LVHM <minh@skoolbo.com>
2017-12-19 21:40:41 +1100

    no message

--- index.js
+++ index.js
@@ -104,14 +104,19 @@ function callSendAPI(sender_psid, response) {
         },
         "message": response
     }
-    function callSendAPI(sender_psid, response) {
-        // Construct the message body
-        let request_body = {
-            "recipient": {
-                "id": sender_psid
-            },
-            "message": response
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
         }
-    }      
+    });
 }
   
